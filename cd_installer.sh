021586c7d2499365939522f328af242b59cbabda
Manibalan <90372350+ManibalanPerumal@users.noreply.github.com>
2021-11-12 23:26:35 +0530

    Update cd_installer.sh

--- cd_installer.sh
+++ cd_installer.sh
@@ -2,10 +2,7 @@ go get -u github.com/ffuf/ffuf
 git clone https://github.com/swarley7/gograbber.git
 cp gograbber/* -r . && rm -fr gograbber
 go build gograbber.go
-git clone https://github.com/C-Sto/recursebuster.git
-cp -r recursebuster/* . && rm -fr recursebuster/
-mv main.go recursebuster.go
-go build recursebuster.go
+go get -u github.com/c-sto/recursebuster
 git clone https://github.com/maurosoria/dirsearch.git
 curl -sL https://raw.githubusercontent.com/epi052/feroxbuster/master/install-nix.sh | bash
 git clone https://github.com/stefanoj3/dirstalk.git
